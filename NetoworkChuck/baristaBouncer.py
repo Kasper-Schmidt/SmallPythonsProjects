b0ed2c5810d74b8afb1b504022eaae5de7544916
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2025-05-12 15:11:33 +0200

    Add files via upload

---
+++ NetoworkChuck/baristaBouncer.py
@@ -0,0 +1,9 @@
+print("Hello, welcome to Schmidt Coffee!")
+
+name = input("What is your name?\n")
+
+if name == "Ben":
+    print("You're not welcome here evil Ben! GET OUT!")
+    exit()
+else:
+    print("Hello " + name + ", Thanks you so much for coming in today.\n\n")
---
+++ NetoworkChuck/if.py
@@ -0,0 +1,12 @@
+if 4 < 3:
+    print("true")
+    print("Still true")
+else:
+    print("false")
+    print("Still false")
+
+
+
+me = "Kasper"
+
+me == "Kasper Schmidt"
